bf9e3f9c05f0fc417618a1e6d881a6cd1d5e3ab6
Mikael Risyad <mikael.s.interns@aux.gojek.com>
2021-11-28 22:58:16 +0700

    fix: button login width to full

--- client/src/components/LoginCard.tsx
+++ client/src/components/LoginCard.tsx
@@ -1,7 +1,7 @@
 const LoginCard = () => {
     return (
         <>
-            <div className="grid">
+            <div className="grid p-5">
                 <h1>Login</h1>
                 <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                     <div className="mb-4">
@@ -17,12 +17,9 @@ const LoginCard = () => {
                         <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" />
                         <p className="text-red-500 text-xs italic">Silahkan Masukan Password</p>
                     </div>
-                    <div className="flex items-center justify-between">
-                        <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    <div className="flex items-center">
+                        <button className="w-full bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                             Login                        </button>
-                        <a className="block align-baseline font-bold text-sm text-yellow-500 hover:text-yellow-800 p-3" href="#sd">
-                            Forgot Password?
-                        </a>
                     </div>
                 </form>
             </div >
