dd16d50d7fdb276b685f113fc0efb795e549bb23
mikelrisyad <mikelrisyad@gmail.com>
2021-12-12 04:29:14 +0700

    feat: default card should be rendered

--- client/src/components/PlayerList.tsx
+++ client/src/components/PlayerList.tsx
@@ -4,6 +4,7 @@ import data from "../data/opponent.json"
 const PlayerList = (level: {level: string | undefined}) => {
     const filterPlayer = data.filter(player => player.level === level.level)
     console.log(filterPlayer)
+    
     const playerList = filterPlayer.map((e) => {
         return (
             <div key={e.id}>
@@ -12,10 +13,20 @@ const PlayerList = (level: {level: string | undefined}) => {
         )
     })
 
+    const defaultCard = data.map((e) => {
+        return (
+            <div key={e.id}>
+                <PlayerCard level={e.level} bio={e.bio} name={e.name} />
+            </div>
+        )
+    })
+
+    const isSelected = level.level !== "";
+
     return (
         <>
             <div className="grid grid-cols-4 grid-flow-row auto-rows-max mx-10 my-3 px-10">
-                {playerList}
+                {isSelected? playerList: defaultCard}
             </div>
         </>
     )
